7b8d241e52d3bf446b7b888520473f10978bb983
Irina Fomina <perk.jerk@gmail.com>
2017-07-21 21:18:27 +0300

    add balance game

---
+++ src/games/balance-game.js
@@ -0,0 +1,45 @@
+import { cons } from 'hexlet-pairs';
+import startGame from '..';
+
+const math = require('mathjs');
+
+const getRandomNumber = () => math.randomInt(10, 1000);
+
+const getMin = arr => Math.min(...arr);
+
+const getMax = arr => Math.max(...arr);
+
+const isBalanced = arr => (getMax(arr) - getMin(arr)) <= 1;
+
+const balance = (arr) => {
+  const tmp = arr;
+  let min = tmp.shift();
+  let max = tmp.pop();
+  while ((max - min) > 1) {
+    max -= 1;
+    min += 1;
+  }
+  tmp.push(min, max);
+  return tmp;
+};
+
+const getBalancedNumber = (number) => {
+  const calcRec = (x) => {
+    const sorted = x.sort((a, b) => a - b);
+    if (isBalanced(x)) {
+      return sorted;
+    }
+
+    return calcRec(balance(sorted));
+  };
+
+  const result = calcRec(number.toString().split('').map(n => parseInt(n, 10)));
+  return result.join('');
+};
+
+const game = () => {
+  const description = 'Balance the given number.';
+  startGame(description, cons(getRandomNumber, getBalancedNumber));
+};
+
+export default game;
